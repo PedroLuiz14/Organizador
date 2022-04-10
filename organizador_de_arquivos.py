008ad0b700323c2791caf6dd4aabfc3600244347
Pedro <pedroruass@gmail.com>
2022-04-10 12:53:15 -0300

    Add files via upload

---
+++ organizador_de_arquivos.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+nolvl = "Livro "
+caminho = r'C:\Users\Pedro\Music\teste'
+
+arquivos = []
+for arquivo in os.listdir(caminho):
+    # Monta o caminho completo ate o arquivo
+    nomearquivo = os.path.join(caminho, arquivo)
+
+    # Se o arquivo for arquivo mesmo, inclui na lista
+    if os.path.isfile(nomearquivo):
+        arquivos.append(arquivo)
+
+for arquivo in arquivos:
+    nomearquivo, nlv = os.path.splitext(arquivo)
+    nolv, nlv, flv, elv = (nomearquivo.split('-'))
+
+    nlv = nlv[1:]
+    if os.path.exists(caminho + '/' +nolvl+ nlv):
+        shutil.move(caminho + '/' + arquivo, caminho + '/' + nolvl + nlv)
+    else:
+        os.makedirs(caminho + '/' + nolvl + nlv)
+        shutil.move(caminho + '/' + arquivo, caminho + '/' +nolvl + nlv)
